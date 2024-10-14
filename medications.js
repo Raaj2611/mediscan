547e63ab374d85a2bbe10a2a2d6a848d51ea91ae
Saashwin <your.raajprabu4808@gmail.com>
2024-10-15 00:50:12 +0800

    Initial commit with project files

---
+++ medications.js
@@ -0,0 +1,11 @@
+export const medications = [
+    { name: 'Loratadine', dosage: '10mg', supplier: 'HTM', brand: 'Tablet' },
+    { name: 'Hiruscar', dosage: '200mg', supplier: 'HTM', brand: 'medinova' },
+    { name: 'CHLORAMINE', dosage: '4mg', supplier: 'HTM', brand: 'DUOPHARMA' },
+    { name: 'Patanol', dosage: '5ml', supplier: 'HTM', brand: 'NOVATIS' },
+    { name: 'BLACKMORES', dosage: '1000mg', supplier: 'HTM', brand: 'FISH OIL' },
+    { name: 'UV Extra Brightening', dosage: '120ml', supplier: 'HTM', brand: 'Vaseline' },
+    { name: 'BARRIER MOISTURE GEL', dosage: '30g', supplier: 'HTM', brand: 'SKINTIFIC' },
+    { name: 'HYDRA ROSE', dosage: '', supplier: 'HTM', brand: 'Wardah' },
+    { name: 'MAKEUP SETTING SPRAY', dosage: '60ml', supplier: 'HTM', brand: 'DAZZLE ME' },
+];
