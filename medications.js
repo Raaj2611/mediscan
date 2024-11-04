a8ebb878ced6741c24c9a504679c086f54343456
Raaj2611 <raajprabu4808@gmail.com>
2024-11-04 22:28:21 +0800

    Update medications.js

--- medications.js
+++ medications.js
@@ -1,7 +1,7 @@
 export const medications = [
     { name: 'Loratadine', dosage: '10mg', supplier: 'HTM', brand: 'Tablet' },
     { name: 'Hiruscar', dosage: '200mg', supplier: 'HTM', brand: 'medinova' },
-    { name: 'CHLORAMINE', dosage: '4mg', supplier: 'HTM', brand: 'DUOPHARMA' },
+    { name: 'night moisturizer', dosage: 'no dosage', supplier: 'HTM', brand: 'Wardah' },
     { name: 'Patanol', dosage: '5ml', supplier: 'HTM', brand: 'NOVATIS' },
     { name: 'BLACKMORES', dosage: '1000mg', supplier: 'HTM', brand: 'FISH OIL' },
     { name: 'UV Extra Brightening', dosage: '120ml', supplier: 'HTM', brand: 'Vaseline' },
